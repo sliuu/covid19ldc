8668408ae284bb323cf47c7d1e869f0800290b9c
Stephanie Liu <sliu14@outlook.com>
2020-09-11 08:46:30 -0400

    Fix routing with next.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,30 @@
-const withPlugins = require("next-compose-plugins");
-const withImages = require("next-images");
-const withSass = require("@zeit/next-sass");
-const webpack = require("webpack");
-const path = require("path");
+// This file is not going through babel transformation.
+// So, we write it in vanilla JS
+// (But you could use ES2015 features supported by your Node.js version)
 
-module.exports = withPlugins([[withSass], [withImages]], {
-  webpack(config, options) {
-    config.resolve.modules.push(path.resolve("./"));
-    config.module.rules.push({
-      test: /\.csv$/,
-      loader: 'raw-loader',
-    });
-    return config;
-  }
-});
+const debug = process.env.NODE_ENV !== "production";
+
+module.exports = {
+  exportPathMap: function () { // /Next-React-Components
+    return {
+      "/": { page: "/" },
+      "/ap-grid-layout": { page: "/ap-grid-layout" },
+      "/ap-highlight": { page: "/ap-highlight" },
+    }
+  },
+  assetPrefix: !debug ? 'https://sliuu.github.io/covid19ldc/' : '',
+  /* webpack: (config, { dev }) => {
+    // Perform customizations to webpack config
+    // console.log('webpack');
+    // console.log(config.module.rules, dev);
+    // Important: return the modified config
+    return config
+  },
+  webpackDevMiddleware: (config) => {
+    // Perform customizations to webpack dev middleware config
+    // console.log('webpackDevMiddleware');
+    // console.log(config);
+    // Important: return the modified config
+    return config
+  }, */
+}
